f439acdbb86e627ab5e0c42fa60874dfea7fa151
melroylobo <melroyjoysonlobo@yahoo.com>
2020-01-02 14:51:13 +0530

    commiting word pair app Jan 02 2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,169 +1,18 @@
+
 import 'package:flutter/material.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  //int _counter = 0;
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-  final SnackBar snackBar = const SnackBar(content: Text('Showing Snackbar'));
-
-  void openPage(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(
-      builder: (BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Next page'),
-          ),
-          body: const Center(
-            child: Text(
-              'This is the next page',
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-        );
-      },
-    ));
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      // _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.wifi_lock),
-            tooltip: 'Show Snackbar',
-            color: Colors.black,
-            highlightColor: Colors.red,
-            onPressed: () {
-              scaffoldKey.currentState.showSnackBar(snackBar);
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.navigate_next),
-            tooltip: 'Next page',
-            onPressed: () {
-              openPage(context);
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Text('Hello World'),
-            ),
-            Image.asset('assets/images/guitar.jpg'),
-            Icon(
-              Icons.star,
-              color: Colors.red[500],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset('assets/images/guitar.jpg'),
-                Image.asset('assets/images/guitar.jpg'),
-                Image.asset('assets/images/guitar.jpg'),
-              ],
-            ),
-//            Row(
-//              children: <Widget>[
-//                Expanded(
-//                  child: Text('Deliver features faster',
-//                      textAlign: TextAlign.center,
-//                      style: TextStyle(fontWeight: FontWeight.bold)),
-//                ),
-//                Expanded(
-//                  child:
-//                      Text('Craft beautiful UIs', textAlign: TextAlign.center),
-//                ),
-//                Expanded(
-//                  child: FittedBox(
-//                    fit: BoxFit.contain, // otherwise the logo will be tiny
-//                    child: const FlutterLogo(),
-//                  ),
-//                ),
-//              ],
-//
-//            ),
 
 
-            Column(
 
-
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text('We move under cover and we move as one'),
-
-              ],
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
